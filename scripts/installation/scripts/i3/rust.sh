2dc2fab72e5949b21f4f27e4aedf4dc6009609ab
ritzier <ritzierwheat@gmail.com>
2026-08-07 02:19:21 +0800

    refactor(i3config): update LspMux configuration and installation script
    - `i3config/.config/lspmux/config.toml`:
        - Remove unused configuration file.
    
    - `scripts/installation/scripts/i3/rust.sh`:
        - Update LspMux configuration to use dynamic socket paths.
        - Ensure proper enabling of LspMux service or socket based on availability.

--- scripts/installation/scripts/i3/rust.sh
+++ scripts/installation/scripts/i3/rust.sh
@@ -38,6 +38,30 @@ fi
 # -----------------------------
 cargo binstall cargo-leptos leptosfmt -y
 
-# Lspmux
+# -----------------------------
+# LspMux
+# -----------------------------
+
+LSPMUX_DIR="$HOME/.config/lspmux"
+
+mkdir -p "$LSPMUX_DIR"
+
+cat >"$LSPMUX_DIR/config.toml" <<EOF
+instance_timeout = 600
+gc_interval = 15
+
+listen = "$LSPMUX_DIR/lspmux.sock"
+connect = "$LSPMUX_DIR/lspmux.sock"
+
+log_filters = "info"
+
+pass_environment = ["PATH"]
+EOF
+
 systemctl --user daemon-reload
-systemctl --user enable --now lspmux
+
+if systemctl --user list-unit-files | grep -q lspmux.socket; then
+    systemctl --user enable --now lspmux.socket
+else
+    systemctl --user enable --now lspmux.service
+fi
