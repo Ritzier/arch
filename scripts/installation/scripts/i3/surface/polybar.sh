4ccf3c3bee5439d9ef0a82f005df4c316a83ea3c
ritzier <ritzierwheat@gmail.com>
2026-08-08 17:36:35 +0800

    fix(polybar.sh): update module configuration
    - `scripts/installation/scripts/i3/surface/polybar.sh`:
        - Comment out the 12tbfilesystem version.
        - Uncomment the battery-script version.

--- scripts/installation/scripts/i3/surface/polybar.sh
+++ scripts/installation/scripts/i3/surface/polybar.sh
@@ -2,14 +2,9 @@ file="$HOME/.config/polybar/config.ini"
 
 echo "[+] Updating Polybar modules-right..."
 
-# Comment the 12tbfilesystem version
 sed -i \
-    's/^modules-right = left cpu memory filesystem 12tbfilesystem 10tbfilesystem right$/# modules-right = left cpu memory filesystem 12tbfilesystem 10tbfilesystem right/' \
-    "$file"
-
-# Uncomment battery-script version
-sed -i \
-    's/^# modules-right = left cpu memory filesystem battery-script right$/modules-right = left cpu memory filesystem battery-script right/' \
-    "$file"
+    -e 's/^modules-right = left cpu memory filesystem 12tbfilesystem right$/# modules-right = left cpu memory filesystem 12tbfilesystem right/' \
+    -e 's/^# modules-right = left cpu memory filesystem battery-script right$/modules-right = left cpu memory filesystem battery-script right/' \
+    $file
 
 echo "[✓] Polybar modules updated"
